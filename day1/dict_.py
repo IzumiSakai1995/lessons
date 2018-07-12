f53c93379e236de1c0a849a7cecb896d5f834351
IzumiSakai1995 <IzumiSakai1995@gmail.com>
2018-07-12 14:32:52 +0800

    update

---
+++ day1/dict_.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/07/12 14:15:28
+# @Author  : Izumi Sakai
+# @File    : dict_.py
+# @Software: PyCharm
+
+dict_demo = {'Izumi':'23','Joe':'22'}
+# print(dict_demo['Izumi'])
+
+#判断是否存在字典中
+# print(dict_demo['Yuri'])
+# print('Izumi' in dict_demo)
+# print(dict_demo.get('Yuri'))
+dict_demo2 = dict_demo
